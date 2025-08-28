324ebdabd268286db39ee1bee143683e146a6bb7
Shivani Naroju <142157246+Shivaninaroju@users.noreply.github.com>
2025-08-28 17:45:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from utils import load_pdf, split_documents, create_vector_db, load_vector_db
 
 load_dotenv()
 app = FastAPI()
+
 # Allow frontend on localhost:5173 (Vite)
 app.add_middleware(
     CORSMiddleware,
